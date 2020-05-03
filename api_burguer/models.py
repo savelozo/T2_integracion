1a095ccab18d73de94cfee8509ff5bfcc79212d7
Seba Velozo <savelozo@uc.cl>
2020-05-02 20:17:34 -0400

    Finish all ingredients methods

--- api_burguer/models.py
+++ api_burguer/models.py
@@ -3,20 +3,20 @@ from django.db import models
 class Ingredient(models.Model):
 
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=256)
-    description = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    descripcion = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.name
+        return self.nombre
 
 class Burguer(models.Model):
 
-    ingredients = models.ManyToManyField(Ingredient)
     id = models.IntegerField(primary_key=True)
-    price = models.IntegerField()
-    name = models.CharField(max_length=256)
-    description = models.CharField(max_length=256)
-    image = models.CharField(max_length=256)
+    ingredientes = models.ManyToManyField(Ingredient)
+    precio = models.IntegerField()
+    descripcion = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    imagen = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.name
+        return self.nombre
--- api_burguer/urls.py
+++ api_burguer/urls.py
@@ -1,12 +1,25 @@
 
 from django.urls import path, include
 from . import views
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('ingrediente', views.IngredientView)
-#router.register('hamburguesa', views.IngredientView)
+#from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('ingrediente/', views.ingredient_list),
+    path('ingrediente/<id>', views.ingredient_detail),
+    path('hamburguesa/', views.burguer_list),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+
+# from django.urls import path, include
+# from . import views
+#
+#
+# urlpatterns = [
+#     path('ingrediente/', views.IngredientView),
+#     path('hamburguesa/', views.BurguerView),
+# ]
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
